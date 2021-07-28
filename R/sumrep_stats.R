8990367d91d1737e686729723fb7c8dded8e2aba
Georgina Leslie <georgieleslie@hotmail.com>
2021-07-28 17:06:26 +0100

    Added initial sumrep functions for pairwise distance | created and linked stats_data to hold expected values for mean , min , max , quantiles etc | checked how repertoires are accessed when uploaded|

--- R/sumrep_stats.R
+++ R/sumrep_stats.R
@@ -18,14 +18,50 @@ GenomeFileUploaded <- function(){
 }
 
 
-pairwiseDistanceInfo <- function(file_data_table){
+#' readInStatsData reads in the stats_data.csv file containing the min max values 
+#' and other statistical values that are used to determine red and amber values in 
+#' repertoires.
+#' 
+#' *PLEASE NOTE*
+#' The stats used were generated from Healthy control sets. If your repertoire has a
+#' range of data from people from other areas they may mean the values given
+#' are incorrect 
+#'
+readInStatsData <- function(function_nm){
+  stats_data <- read.csv("../../../../../R/stats_data.csv")
+  stats_data_row = stats_data[stats_data$function_name==function_nm,]
+  return(stats_data)
   
-  pairwise_distance = getPairwiseDistanceDistribution()
+}
+
+
+pairwiseDistanceInfo <- function(data){
+  
+  pairwise_distance = getPairwiseDistanceDistribution(data)
+  
   
   hist(pairwise_distance)
-  min(pairwise_distance)
-  max(pairwise_distance)
+  min_val <- min(pairwise_distance)
+  max_val <- max(pairwise_distance)
+  quantile_5 <- unname(quantile(pairwise_distance,probs=c(0.05,0.95)))[1]
+  quantile_95 <- unname(quantile(pairwise_distance,probs=c(0.05,0.95)))[2]
+  ###
   
+  stats_data  <- readInStatsData("pairwiseDistanceInfo")
+  print(stats_data)
+  exp_min <- stats_data[1,2]
+  exp_max <- stats_data[1,3]
+  
+  exp_perc_5 <- stats_data[1,6]
+  exp_perc_95 <- stats_data[1,7]
+  
+  ##
+  
+  hist(pairwise_distance)
+  abline(v=exp_perc_5,col=" orange")
+  abline(v=exp_perc_95,col="orange")
+  abline(v=quantile_5,col="blue")
+  abline(v=quantile_95,col="blue")
   
   
 }
